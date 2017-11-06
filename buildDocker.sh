76921fbd1dd4002af2308816347f6c8e0ea93396
Your Name <angular2guy@gmail.com>
2017-11-06 20:15:24 +0100

    maven build with docker

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -f src/docker/Dockerfile -t angular2guy:trader  .
-docker run -p 8080:8080 --network="host" angular2guy:trader
+docker build -t angular2guy/trader:latest  .
+docker run -p 8080:8080 --network="host" angular2guy/trader:latest
