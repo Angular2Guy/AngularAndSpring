e8cd35dec3e62ddf93bf8fb581f8adfaee514ec2
Your Name <angular2guy@gmail.com>
2017-12-01 19:18:08 +0100

    update to Milestone 7 and first step for security

--- src/main/java/ch/xxx/trader/TraderApplication.java
+++ src/main/java/ch/xxx/trader/TraderApplication.java
@@ -17,8 +17,13 @@ package ch.xxx.trader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -26,6 +31,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TraderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TraderApplication.class, args);		
+		SpringApplication.run(TraderApplication.class, args);
+	}
+
+	@Configuration
+	@Order(SecurityProperties.DEFAULT_FILTER_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic().disable();
+			http.authorizeRequests().anyRequest().permitAll().anyRequest().anonymous();	
+			http.csrf().disable();
+		}
 	}
 }
