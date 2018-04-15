d6397023ebec801653cee4e0b9ac5ef86241be4f
sven1 <angular2guy@gmail.com>
2018-04-15 08:27:07 +0200

    add daily values to importer

--- src/main/java/ch/xxx/trader/clients/PrepareData.java
+++ src/main/java/ch/xxx/trader/clients/PrepareData.java
@@ -79,7 +79,52 @@ public class PrepareData {
 
 			begin.add(Calendar.DAY_OF_YEAR, 1);
 			end.add(Calendar.DAY_OF_YEAR, 1);
-			log.info("Prepared Bitstamp Data for: " + sdf.format(begin.getTime()));
+			log.info("Prepared Bitstamp Hour Data for: " + sdf.format(begin.getTime()));
+		}
+	}
+
+	// @Scheduled(fixedRate = 300000000, initialDelay = 3000)
+	@Scheduled(cron = "5 0 1 ? * ?")
+	public void createBsDailyAvg() {
+		if (!this.operations.collectionExists("quoteBsDay").block()) {
+			this.operations.createCollection("quoteBsDay").block();
+		}
+		Query query = new Query();
+		query.with(Sort.by("createdAt").ascending());
+		QuoteBs firstQuote = this.operations.findOne(query, QuoteBs.class).block();
+		query = new Query();
+		query.with(Sort.by("createdAt").descending());
+		QuoteBs lastHourQuote = this.operations.findOne(query, QuoteBs.class, "quoteBsDay").block();
+		Calendar globalBeginn = Calendar.getInstance();
+		if (lastHourQuote == null) {
+			globalBeginn.setTime(firstQuote.getCreatedAt());
+		} else {
+			globalBeginn.setTime(lastHourQuote.getCreatedAt());
+		}
+
+		Calendar begin = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		begin.setTime(globalBeginn.getTime());
+		begin.set(Calendar.MINUTE, 0);
+		begin.set(Calendar.SECOND, 0);
+		end.setTime(begin.getTime());
+		end.add(Calendar.DAY_OF_YEAR, 1);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+		Calendar now = Calendar.getInstance();
+		while (end.before(now)) {
+			query = new Query();
+			query.addCriteria(Criteria.where("createdAt").gt(begin.getTime()).lt(end.getTime()));
+			// Bitstamp
+			List<Collection<QuoteBs>> collectBs = this.operations.find(query, QuoteBs.class)
+					.collectMultimap(quote -> quote.getPair(), quote -> quote)
+					.map(multimap -> multimap.keySet().stream().map(key -> makeBsQuoteDay(key, multimap, begin, end))
+							.collect(Collectors.toList()))
+					.block();
+			collectBs.forEach(col -> this.operations.insertAll(Mono.just(col), "quoteBsDay").blockLast());
+
+			begin.add(Calendar.DAY_OF_YEAR, 1);
+			end.add(Calendar.DAY_OF_YEAR, 1);
+			log.info("Prepared Bitstamp Day Data for: " + sdf.format(begin.getTime()));
 		}
 	}
 
@@ -124,7 +169,52 @@ public class PrepareData {
 
 			begin.add(Calendar.DAY_OF_YEAR, 1);
 			end.add(Calendar.DAY_OF_YEAR, 1);
-			log.info("Prepared Bitfinex Data for: " + sdf.format(begin.getTime()));
+			log.info("Prepared Bitfinex Hour Data for: " + sdf.format(begin.getTime()));
+		}
+	}
+
+	// @Scheduled(fixedRate = 300000000, initialDelay = 3000)
+	@Scheduled(cron = "10 0 1 ? * ?")
+	public void createBfDailyAvg() {
+		if (!this.operations.collectionExists("quoteBfDay").block()) {
+			this.operations.createCollection("quoteBfDay").block();
+		}
+		Query query = new Query();
+		query.with(Sort.by("createdAt").ascending());
+		QuoteBs firstQuote = this.operations.findOne(query, QuoteBs.class).block();
+		query = new Query();
+		query.with(Sort.by("createdAt").descending());
+		QuoteBs lastHourQuote = this.operations.findOne(query, QuoteBs.class, "quoteBfDay").block();
+		Calendar globalBeginn = Calendar.getInstance();
+		if (lastHourQuote == null) {
+			globalBeginn.setTime(firstQuote.getCreatedAt());
+		} else {
+			globalBeginn.setTime(lastHourQuote.getCreatedAt());
+		}
+
+		Calendar begin = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		begin.setTime(globalBeginn.getTime());
+		begin.set(Calendar.MINUTE, 0);
+		begin.set(Calendar.SECOND, 0);
+		end.setTime(begin.getTime());
+		end.add(Calendar.DAY_OF_YEAR, 1);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+		Calendar now = Calendar.getInstance();
+		while (end.before(now)) {
+			query = new Query();
+			query.addCriteria(Criteria.where("createdAt").gt(begin.getTime()).lt(end.getTime()));
+			// Bitfinex
+			List<Collection<QuoteBf>> collectBf = this.operations.find(query, QuoteBf.class)
+					.collectMultimap(quote -> quote.getPair(), quote -> quote)
+					.map(multimap -> multimap.keySet().stream().map(key -> makeBfQuoteDay(key, multimap, begin, end))
+							.collect(Collectors.toList()))
+					.block();
+			collectBf.forEach(col -> this.operations.insertAll(Mono.just(col), "quoteBfDay").blockLast());
+
+			begin.add(Calendar.DAY_OF_YEAR, 1);
+			end.add(Calendar.DAY_OF_YEAR, 1);
+			log.info("Prepared Bitfinex Day Data for: " + sdf.format(begin.getTime()));
 		}
 	}
 
@@ -169,7 +259,52 @@ public class PrepareData {
 
 			begin.add(Calendar.DAY_OF_YEAR, 1);
 			end.add(Calendar.DAY_OF_YEAR, 1);
-			log.info("Prepared Itbit Data for: " + sdf.format(begin.getTime()));
+			log.info("Prepared Itbit Hour Data for: " + sdf.format(begin.getTime()));
+		}
+	}
+
+	// @Scheduled(fixedRate = 300000000, initialDelay = 3000)
+	@Scheduled(cron = "15 0 1 ? * ?")
+	public void createIbDailyAvg() {
+		if (!this.operations.collectionExists("quoteIbDay").block()) {
+			this.operations.createCollection("quoteIbDay").block();
+		}
+		Query query = new Query();
+		query.with(Sort.by("createdAt").ascending());
+		QuoteBs firstQuote = this.operations.findOne(query, QuoteBs.class).block();
+		query = new Query();
+		query.with(Sort.by("createdAt").descending());
+		QuoteBs lastHourQuote = this.operations.findOne(query, QuoteBs.class, "quoteIbDay").block();
+		Calendar globalBeginn = Calendar.getInstance();
+		if (lastHourQuote == null) {
+			globalBeginn.setTime(firstQuote.getCreatedAt());
+		} else {
+			globalBeginn.setTime(lastHourQuote.getCreatedAt());
+		}
+
+		Calendar begin = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		begin.setTime(globalBeginn.getTime());
+		begin.set(Calendar.MINUTE, 0);
+		begin.set(Calendar.SECOND, 0);
+		end.setTime(begin.getTime());
+		end.add(Calendar.DAY_OF_YEAR, 1);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+		Calendar now = Calendar.getInstance();
+		while (end.before(now)) {
+			query = new Query();
+			query.addCriteria(Criteria.where("createdAt").gt(begin.getTime()).lt(end.getTime()));
+			// Itbit
+			List<Collection<QuoteIb>> collectIb = this.operations.find(query, QuoteIb.class)
+					.collectMultimap(quote -> quote.getPair(), quote -> quote)
+					.map(multimap -> multimap.keySet().stream().map(key -> makeIbQuoteDay(key, multimap, begin, end))
+							.collect(Collectors.toList()))
+					.block();
+			collectIb.forEach(col -> this.operations.insertAll(Mono.just(col), "quoteIbDay").blockLast());
+
+			begin.add(Calendar.DAY_OF_YEAR, 1);
+			end.add(Calendar.DAY_OF_YEAR, 1);
+			log.info("Prepared Itbit Day Data for: " + sdf.format(begin.getTime()));
 		}
 	}
 
@@ -212,9 +347,81 @@ public class PrepareData {
 
 			begin.add(Calendar.DAY_OF_YEAR, 1);
 			end.add(Calendar.DAY_OF_YEAR, 1);
-			log.info("Prepared Coinbase Data for: " + sdf.format(begin.getTime()) + " Time: "
+			log.info("Prepared Coinbase Hour Data for: " + sdf.format(begin.getTime()) + " Time: "
 					+ (new Date().getTime() - start.getTime()) + "ms");
 		}
+	}
+
+	// @Scheduled(fixedRate = 300000000, initialDelay = 3000)
+	@Scheduled(cron = "20 0 1 ? * ?")
+	public void createCbDailyAvg() {
+		if (!this.operations.collectionExists("quoteCbDay").block()) {
+			this.operations.createCollection("quoteCbDay").block();
+		}
+		Query query = new Query();
+		query.with(Sort.by("createdAt").ascending());
+		QuoteBs firstQuote = this.operations.findOne(query, QuoteBs.class).block();
+		query = new Query();
+		query.with(Sort.by("createdAt").descending());
+		QuoteBs lastHourQuote = this.operations.findOne(query, QuoteBs.class, "quoteCbDay").block();
+		Calendar globalBeginn = Calendar.getInstance();
+		if (lastHourQuote == null) {
+			globalBeginn.setTime(firstQuote.getCreatedAt());
+		} else {
+			globalBeginn.setTime(lastHourQuote.getCreatedAt());
+		}
+
+		Calendar begin = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		begin.setTime(globalBeginn.getTime());
+		begin.set(Calendar.MINUTE, 0);
+		begin.set(Calendar.SECOND, 0);
+		end.setTime(begin.getTime());
+		end.add(Calendar.DAY_OF_YEAR, 1);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+		Calendar now = Calendar.getInstance();
+		while (end.before(now)) {
+			Date start = new Date();
+			query = new Query();
+			query.addCriteria(Criteria.where("createdAt").gt(begin.getTime()).lt(end.getTime()));
+			// Coinbase
+			Collection<QuoteCb> collectCb = this.operations.find(query, QuoteCb.class).collectList()
+					.map(quotes -> makeCbQuoteDay(quotes, begin, end)).block();
+			this.operations.insertAll(Mono.just(collectCb), "quoteCbDay").blockLast();
+
+			begin.add(Calendar.DAY_OF_YEAR, 1);
+			end.add(Calendar.DAY_OF_YEAR, 1);
+			log.info("Prepared Coinbase Day Data for: " + sdf.format(begin.getTime()) + " Time: "
+					+ (new Date().getTime() - start.getTime()) + "ms");
+		}
+	}
+
+	private Collection<QuoteCb> makeCbQuoteDay(List<QuoteCb> quotes, Calendar begin, Calendar end) {
+		List<QuoteCb> hourQuotes = new LinkedList<QuoteCb>();
+		BigDecimal[] params = new BigDecimal[170];
+		Class[] types = new Class[170];
+		for (int x = 0; x < 170; x++) {
+			params[x] = BigDecimal.ZERO;
+			types[x] = BigDecimal.class;
+		}
+		QuoteCb quoteCb = null;
+		try {
+			quoteCb = QuoteCb.class.getConstructor(types).newInstance(params);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			throw new RuntimeException(e);
+		}
+		quoteCb.setCreatedAt(begin.getTime());
+		long count = quotes.stream().filter(quote -> {
+			return quote.getCreatedAt().after(begin.getTime()) && quote.getCreatedAt().before(end.getTime());
+		}).count();
+
+		quoteCb = quotes.stream().filter(quote -> {
+			return quote.getCreatedAt().after(begin.getTime()) && quote.getCreatedAt().before(end.getTime());
+		}).reduce(quoteCb, (q1, q2) -> avgCbQuoteHour(q1, q2, count));
+
+		hourQuotes.add(quoteCb);
+		return hourQuotes;
 	}
 
 	private Collection<QuoteCb> makeCbQuoteHour(List<QuoteCb> quotes, Calendar begin, Calendar end) {
@@ -311,10 +518,28 @@ public class PrepareData {
 			QuoteIb hourQuote = multimap.get(key).stream().filter(quote -> {
 				return quote.getCreatedAt().after(hours.get(x).getTime())
 						&& quote.getCreatedAt().before(hours.get(x + 1).getTime());
-			}).reduce(quoteIb, (q1, q2) -> avgIbQuoteHour(q1, q2, count));
+			}).reduce(quoteIb, (q1, q2) -> avgIbQuote(q1, q2, count));
 			// hourQuote.setPair(key);
 			hourQuotes.add(hourQuote);
 		}
+		return hourQuotes;
+	}
+
+	private Collection<QuoteIb> makeIbQuoteDay(String key, Map<String, Collection<QuoteIb>> multimap, Calendar begin,
+			Calendar end) {
+		List<QuoteIb> hourQuotes = new LinkedList<QuoteIb>();
+		QuoteIb quoteIb = new QuoteIb(key, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO,
+				BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO,
+				BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, new Date());
+		quoteIb.setCreatedAt(begin.getTime());
+		long count = multimap.get(key).stream().filter(quote -> {
+			return quote.getCreatedAt().after(begin.getTime()) && quote.getCreatedAt().before(end.getTime());
+		}).count();
+		QuoteIb hourQuote = multimap.get(key).stream().filter(quote -> {
+			return quote.getCreatedAt().after(begin.getTime()) && quote.getCreatedAt().before(end.getTime());
+		}).reduce(quoteIb, (q1, q2) -> avgIbQuote(q1, q2, count));
+		// hourQuote.setPair(key);
+		hourQuotes.add(hourQuote);
 		return hourQuotes;
 	}
 
@@ -347,10 +572,48 @@ public class PrepareData {
 			QuoteBf hourQuote = multimap.get(key).stream().filter(quote -> {
 				return quote.getCreatedAt().after(hours.get(x).getTime())
 						&& quote.getCreatedAt().before(hours.get(x + 1).getTime());
-			}).reduce(quoteBf, (q1, q2) -> avgBfQuoteHour(q1, q2, count));
+			}).reduce(quoteBf, (q1, q2) -> avgBfQuote(q1, q2, count));
 			hourQuote.setPair(key);
 			hourQuotes.add(hourQuote);
 		}
+		return hourQuotes;
+	}
+
+	private Collection<QuoteBf> makeBfQuoteDay(String key, Map<String, Collection<QuoteBf>> multimap, Calendar begin,
+			Calendar end) {
+		List<QuoteBf> hourQuotes = new LinkedList<QuoteBf>();
+
+		QuoteBf quoteBf = new QuoteBf(BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO,
+				BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, "");
+		quoteBf.setCreatedAt(begin.getTime());
+		long count = multimap.get(key).stream().filter(quote -> {
+			return quote.getCreatedAt().after(begin.getTime()) && quote.getCreatedAt().before(end.getTime());
+		}).count();
+		QuoteBf hourQuote = multimap.get(key).stream().filter(quote -> {
+			return quote.getCreatedAt().after(begin.getTime()) && quote.getCreatedAt().before(end.getTime());
+		}).reduce(quoteBf, (q1, q2) -> avgBfQuote(q1, q2, count));
+		hourQuote.setPair(key);
+		hourQuotes.add(hourQuote);
+
+		return hourQuotes;
+	}
+
+	private Collection<QuoteBs> makeBsQuoteDay(String key, Map<String, Collection<QuoteBs>> multimap, Calendar begin,
+			Calendar end) {
+		List<QuoteBs> hourQuotes = new LinkedList<QuoteBs>();
+
+		QuoteBs quoteBs = new QuoteBs(BigDecimal.ZERO, BigDecimal.ZERO, begin.getTime(), BigDecimal.ZERO,
+				BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO);
+		quoteBs.setCreatedAt(begin.getTime());
+		long count = multimap.get(key).stream().filter(quote -> {
+			return quote.getCreatedAt().after(begin.getTime()) && quote.getCreatedAt().before(end.getTime());
+		}).count();
+		QuoteBs hourQuote = multimap.get(key).stream().filter(quote -> {
+			return quote.getCreatedAt().after(begin.getTime()) && quote.getCreatedAt().before(end.getTime());
+		}).reduce(quoteBs, (q1, q2) -> avgBsQuote(q1, q2, count));
+		hourQuote.setPair(key);
+		hourQuotes.add(hourQuote);
+
 		return hourQuotes;
 	}
 
@@ -370,14 +633,14 @@ public class PrepareData {
 			QuoteBs hourQuote = multimap.get(key).stream().filter(quote -> {
 				return quote.getCreatedAt().after(hours.get(x).getTime())
 						&& quote.getCreatedAt().before(hours.get(x + 1).getTime());
-			}).reduce(quoteBs, (q1, q2) -> avgBsQuoteHour(q1, q2, count));
+			}).reduce(quoteBs, (q1, q2) -> avgBsQuote(q1, q2, count));
 			hourQuote.setPair(key);
 			hourQuotes.add(hourQuote);
 		}
 		return hourQuotes;
 	}
 
-	private QuoteIb avgIbQuoteHour(QuoteIb q1, QuoteIb q2, long count) {
+	private QuoteIb avgIbQuote(QuoteIb q1, QuoteIb q2, long count) {
 		QuoteIb myQuote = new QuoteIb(q1.getPair(), avgHourValue(q1.getBid(), q2.getBid(), count),
 				avgHourValue(q1.getBidAmt(), q2.getBidAmt(), count), avgHourValue(q1.getAsk(), q2.getAsk(), count),
 				avgHourValue(q1.getAskAmt(), q2.getAskAmt(), count),
@@ -394,7 +657,7 @@ public class PrepareData {
 		return myQuote;
 	}
 
-	private QuoteBs avgBsQuoteHour(QuoteBs q1, QuoteBs q2, long count) {
+	private QuoteBs avgBsQuote(QuoteBs q1, QuoteBs q2, long count) {
 		QuoteBs myQuote = new QuoteBs(avgHourValue(q1.getHigh(), q2.getHigh(), count),
 				avgHourValue(q1.getLast(), q2.getLast(), count), q1.getTimestamp(),
 				avgHourValue(q1.getBid(), q2.getBid(), count), avgHourValue(q1.getVwap(), q2.getVwap(), count),
@@ -404,7 +667,7 @@ public class PrepareData {
 		return myQuote;
 	}
 
-	private QuoteBf avgBfQuoteHour(QuoteBf q1, QuoteBf q2, long count) {
+	private QuoteBf avgBfQuote(QuoteBf q1, QuoteBf q2, long count) {
 		QuoteBf myQuote = new QuoteBf(avgHourValue(q1.getMid(), q2.getMid(), count),
 				avgHourValue(q1.getBid(), q2.getBid(), count), avgHourValue(q1.getAsk(), q2.getAsk(), count),
 				avgHourValue(q1.getLast_price(), q2.getLast_price(), count),
