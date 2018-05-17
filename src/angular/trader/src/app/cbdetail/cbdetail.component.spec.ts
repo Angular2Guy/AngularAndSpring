85cdadc112e036a169946b3e04be30695d355a76
Angular2Guy <angular2guy@gmail.com>
2018-05-17 20:43:38 +0200

    Add Tests

--- src/angular/trader/src/app/cbdetail/cbdetail.component.spec.ts
+++ src/angular/trader/src/app/cbdetail/cbdetail.component.spec.ts
@@ -89,9 +89,36 @@ describe('CbdetailComponent', () => {
   it('should have value', () => {
       expect(component.currQuote.aed).toBe(30446.14);
   });
-//  it('should show usd', () => {
-//      const de: DebugElement = fixture.debugElement;
-//      const el: HTMLElement = de.query(By.css('#usd')).nativeElement;      
-//      expect(el.textContent).toEqual('2.00');      
-//  });
+  it('should show usd', () => {
+      component.currpair = 'btcusd';
+      fixture.autoDetectChanges();
+      const de: DebugElement = fixture.debugElement;
+      const el: HTMLElement = de.query(By.css('#usd')).nativeElement;
+      expect(el.textContent).toEqual('8,288.78');      
+  });
+  it('should show eur', () => {
+      component.currpair = 'btcusd';
+      fixture.autoDetectChanges();
+      const de: DebugElement = fixture.debugElement;
+      const el: HTMLElement = de.query(By.css('#eur')).nativeElement;
+      expect(el.textContent).toEqual('7,031.92');      
+  });
+  it('should show gbp', () => {
+      component.currpair = 'btcusd';
+      fixture.autoDetectChanges();
+      const de: DebugElement = fixture.debugElement;
+      const el: HTMLElement = de.query(By.css('#gbp')).nativeElement;
+      expect(el.textContent).toEqual('6,168.36');      
+  });
+  it('should show createdAt', () => {
+      component.currpair = 'btcusd';
+      fixture.autoDetectChanges();
+      const de: DebugElement = fixture.debugElement;
+      const el: HTMLElement = de.query(By.css('#createdAt')).nativeElement;
+      const myDate = new Date('2018-05-16T19:20:26');
+      let dateStr = (myDate.getHours().toString().length === 1 ? '0'+ myDate.getHours() : myDate.getHours()) 
+      +':'+(myDate.getMinutes().toString().length === 1 ? '0' + myDate.getMinutes() : myDate.getMinutes())
+      +':'+(myDate.getSeconds().toString().length === 1 ? '0' + myDate.getSeconds() : myDate.getSeconds());
+      expect(el.textContent).toEqual(dateStr);      
+  });
 });
