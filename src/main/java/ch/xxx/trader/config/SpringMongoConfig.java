68ae407d6a91fbdc41e3feb6a0cfecdaf86a7162
Angular2Guy <angular2guy@gmail.com>
2018-08-19 23:14:17 +0200

    fix

--- src/main/java/ch/xxx/trader/config/SpringMongoConfig.java
+++ src/main/java/ch/xxx/trader/config/SpringMongoConfig.java
@@ -1,14 +1,21 @@
 package ch.xxx.trader.config;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 import com.mongodb.MongoClient;
 
+import ch.xxx.trader.data.ScheduledTask;
+
 @Configuration
 public class SpringMongoConfig extends AbstractMongoConfiguration {
-
+	private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
+	
     @Value("${spring.data.mongodb.host}")
     private String mongoHost;
 
@@ -21,6 +28,7 @@ public class SpringMongoConfig extends AbstractMongoConfiguration {
 	@Override
 	public MongoClient mongoClient() {	
 		String myHost = System.getenv("MONGODB_HOST");
+		log.info("MONGODB_HOST="+myHost);		
 		return new MongoClient(myHost != null ? myHost : mongoHost + ":" + mongoPort);
 	}
 
