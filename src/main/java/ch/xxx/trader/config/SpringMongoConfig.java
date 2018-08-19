a7756cd1b722702a5316781a5c3312288391c44a
Angular2Guy <angular2guy@gmail.com>
2018-08-20 00:05:15 +0200

    fixes

--- src/main/java/ch/xxx/trader/config/SpringMongoConfig.java
+++ src/main/java/ch/xxx/trader/config/SpringMongoConfig.java
@@ -1,19 +1,18 @@
 package ch.xxx.trader.config;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 
-import com.mongodb.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
 
 import ch.xxx.trader.data.ScheduledTask;
 
 @Configuration
-public class SpringMongoConfig extends AbstractMongoConfiguration {
+public class SpringMongoConfig extends AbstractReactiveMongoConfiguration {
 	private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
 	
     @Value("${spring.data.mongodb.host}")
@@ -24,17 +23,17 @@ public class SpringMongoConfig extends AbstractMongoConfiguration {
 
     @Value("${spring.data.mongodb.database}")
     private String mongoDB;
-	
-	@Override
-	public MongoClient mongoClient() {	
-		String myHost = System.getenv("MONGODB_HOST");
-		log.info("MONGODB_HOST="+myHost);		
-		return new MongoClient(myHost != null ? myHost : mongoHost + ":" + mongoPort);
-	}
 
 	@Override
 	protected String getDatabaseName() {
 		return mongoDB;
 	}
+
+	@Override
+	public MongoClient reactiveMongoClient() {
+		String myHost = System.getenv("MONGODB_HOST");		
+		log.info("MONGODB_HOST="+myHost);			
+		return MongoClients.create("mongodb://"+(myHost==null ? mongoHost : myHost)+":"+mongoPort);
+	}	
 
 }
