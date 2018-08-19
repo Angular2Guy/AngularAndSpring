c15d44b3c71e4f23ca94ba4328c90b886b201bbd
Angular2Guy <angular2guy@gmail.com>
2018-08-19 11:32:43 +0200

    fix docker build

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-docker build -t angular2guy/trader:latest  .
+docker run -p 27017:27017 mongo:3.6.6
+docker build -t angular2guy/trader:latest --build-arg JAR_FILE=trader-0.0.1-SNAPSHOT.jar --no-cache .
 docker run -p 8080:8080 --network="host" angular2guy/trader:latest
--- runDocker.sh
+++ runDocker.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+docker run -p 27017:27017 mongo:3.6.6
 docker run -p 8080:8080 --network="host" angular2guy/trader:latest
