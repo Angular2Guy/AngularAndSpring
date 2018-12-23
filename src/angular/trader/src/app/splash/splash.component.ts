0bcb044765da1b9be0c8e961ed29528708a22e94
Angular2Guy <angular2guy@gmail.com>
2018-12-23 14:30:24 +0100

    Add Fade in

--- src/angular/trader/src/app/splash/splash.component.ts
+++ src/angular/trader/src/app/splash/splash.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { trigger, state, animate, transition, style } from '@angular/animations';
 
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.component.html',
-  styleUrls: ['./splash.component.scss']
+  styleUrls: ['./splash.component.scss'],
+  animations: [
+               trigger( 'showSplash', [
+                   state( 'true', style( { opacity: 1 } ) ),
+                   state( 'false', style( { opacity: 0 } ) ),
+                   transition( '1 => 0', animate( '750ms' ) ),
+                   transition( '0 => 1', animate( '7500ms' ) )
+               ])]  
 })
-export class SplashComponent implements OnInit {
-
+export class SplashComponent implements OnInit,AfterViewInit {    
+  myState = false;
+  
   constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() {      
       this.router.navigateByUrl("overview");
+  }
+  
+  ngAfterViewInit(): void {
+      setTimeout(() => this.myState = true);
   }
 
 }
