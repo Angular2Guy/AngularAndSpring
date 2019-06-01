2d8fac924bc3608a7df9fb364a8aa0bb9d03a752
Angular2Guy <angular2guy@gmail.com>
2019-06-01 14:50:59 +0200

    improve filter

--- src/main/java/ch/xxx/trader/jwt/JwtTokenFilter.java
+++ src/main/java/ch/xxx/trader/jwt/JwtTokenFilter.java
@@ -23,28 +23,32 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 public class JwtTokenFilter extends GenericFilterBean {
-
+	private final Logger LOG = LoggerFactory.getLogger(JwtTokenFilter.class);
 	private JwtTokenProvider jwtTokenProvider;
 
-	  public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
-	    this.jwtTokenProvider = jwtTokenProvider;
-	  }
+	public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+		this.jwtTokenProvider = jwtTokenProvider;
+	}
 
-	  @Override
-	  public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
-	      throws IOException, ServletException {
-	    
-	    String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
-	    if (token != null && jwtTokenProvider.validateToken(token)) {
-	      Authentication auth = token != null ? jwtTokenProvider.getAuthentication(token) : null;
-	      SecurityContextHolder.getContext().setAuthentication(auth);
-	    }
-	    filterChain.doFilter(req, res);
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
+			throws IOException, ServletException {
+
+		HttpServletRequest httpReq = (HttpServletRequest) req;
+		String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
+		if (httpReq.getRequestURL().toString().contains("/orderbook") && token != null && jwtTokenProvider.validateToken(token)) {
+			Authentication auth = token != null ? jwtTokenProvider.getAuthentication(token) : null;
+			SecurityContextHolder.getContext().setAuthentication(auth);
+		}
+
+		filterChain.doFilter(req, res);
 	}
 
 }
