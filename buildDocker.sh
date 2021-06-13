a173bb663e73b450898753f670c1ee5f02d3fc5f
Angular2Guy <angular2guy@gmail.com>
2021-06-13 04:08:41 +0200

    update

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+#./mvnw clean install -Ddocker=true -Dnpm.test.script=test-chromium
+./mvnw clean install -Ddocker=true
 docker build -t angular2guy/angularandspring:latest --build-arg JAR_FILE=angularandspring-0.0.1-SNAPSHOT.jar --no-cache .
 docker run -p 8080:8080 --memory="1g" --network="host" angular2guy/angularandspring:latest
