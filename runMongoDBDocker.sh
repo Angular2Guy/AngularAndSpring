9aaf91a33ada3ab758aaafcfb1a0b86bbda353c5
Angular2Guy <angular2guy@gmail.com>
2021-06-21 09:06:16 +0200

    update

--- runMongoDBDocker.sh
+++ runMongoDBDocker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker pull mongo:3.6
 docker run --name local-mongo -p 27017:27017 mongo:3.6
+#docker run --name local-mongo -p 27017:27017 -v ~mongodb:/data/db mongo:3.6
 #docker start local-mongo
 #docker stop local-mongo
