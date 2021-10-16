8b031231487c90d8e068a0c6bb06934de203b906
Angular2Guy <angular2guy@gmail.com>
2021-10-16 20:02:53 +0200

    grafana script

---
+++ runGrafana.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+MYID=id -u
+#data will be stored in ~/tmp/data
+#mkdir ~/tmp/data
+MYDATA=echo ~/tmp/data
+docker run --user $MYID --volume "$MYID:$MYDATA" --network "host" grafana/grafana
