911b59632d229ddb7ebd938caaa1c2fbd6e2920c
Angular2Guy <angular2guy@gmail.com>
2022-02-05 02:23:26 +0100

    fix: add command

--- minikube/minikubeSetup.sh
+++ minikube/minikubeSetup.sh
@@ -9,6 +9,7 @@ minikube config set cpu 2
 minikube config set driver docker
 minikube addons list
 minikube addons enable metrics-server
+kubectl edit deployment -n kube-system metrics-server
 
 kubectl expose pod <pod-name> --port=27017 --type="NodePort"
 mongorestore --gzip mongodb://192.168.49.2:<exposed-port>
