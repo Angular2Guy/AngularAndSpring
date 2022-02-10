c92428439667f63b1ea999145b413ba1ecdbf983
Angular2Guy <angular2guy@gmail.com>
2022-02-10 18:16:22 +0100

    fix: restricted resources config

--- runMongoDBDocker.sh
+++ runMongoDBDocker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker pull mongo:4.4
 docker run --name local-mongo -p 27017:27017 --cpus=2.0 --memory=3g mongo:4.4 --wiredTigerCacheSizeGB 2.0
-#docker run --name local-mongo -p 27017:27017 --cpus=2.0 --memory=3g -v <mongo-dir>:/data/db mongo:4.4 --wiredTigerCacheSizeGB 2.0
+#docker run --name local-mongo -p 27017:27017 --cpus=1.0 --memory=2g -v <mongo-dir>:/data/db mongo:4.4 --wiredTigerCacheSizeGB 1.0
 #docker start local-mongo
 #docker stop local-mongo
 #docker exec -it local-mongo bash
