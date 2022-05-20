e20b6bac69a8e5fd225673136364ff5d203d8c1e
Angular2Guy <angular2guy@gmail.com>
2022-05-20 15:18:14 +0200

    feat: add helm

--- minikube/helmCommand.sh
+++ minikube/helmCommand.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #helm install angularandspring ./angularandspring --set serviceType=NodePort
-#helm install kafka ./kafka 
+#helm install kafka ./kafka
+#helm install angularandspringwithkafka ./angularandspringwithkafka  
