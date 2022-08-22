539980267dba7db94f1183d4bfab9ed144432327
Angular2Guy <angular2guy@gmail.com>
2022-08-22 23:39:32 +0200

    feat: update

--- backend/src/main/java/ch/xxx/trader/adapter/config/GlobalExceptionHandler.java
+++ backend/src/main/java/ch/xxx/trader/adapter/config/GlobalExceptionHandler.java
@@ -26,17 +26,20 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.mongodb.MongoTimeoutException;
+
+import io.netty.handler.timeout.TimeoutException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-	@ExceptionHandler({ RequestRejectedException.class })
+	@ExceptionHandler({ MongoTimeoutException.class, TimeoutException.class })
 	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 	public Object handleException(final Exception exception, final HttpServletRequest request) {
 		record MyEntry(String key, Object value) {
