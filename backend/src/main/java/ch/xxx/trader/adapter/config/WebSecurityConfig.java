c50aae374ed9a9c78b031b4afed917fe7e21f83b
Angular2guy <angular2guy@gmail.com>
2023-07-24 18:14:40 +0200

    fix: security

--- backend/src/main/java/ch/xxx/trader/adapter/config/WebSecurityConfig.java
+++ backend/src/main/java/ch/xxx/trader/adapter/config/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig {
 				.headers(myHeaders -> myHeaders.contentSecurityPolicy(myCsp -> myCsp.policyDirectives(
 						"default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';")))
 				.headers(myHeaders -> myHeaders.xssProtection(myXss -> myXss.headerValue(HeaderValue.ENABLED)))
+				.headers(myHeaders -> myHeaders.frameOptions(myFo -> myFo.sameOrigin()))
 				.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);				
 		return httpSecurity.build();
 	}
