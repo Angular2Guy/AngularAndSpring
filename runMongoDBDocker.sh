568b6dcd06e8c004f6fb737d0b90e5ea5dfdd9ad
Angular2guy <angular2guy@gmail.com>
2026-02-16 18:25:36 +0100

    fix: update mongodb

--- runMongoDBDocker.sh
+++ runMongoDBDocker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker pull mongo:6.0
-docker run --name local-mongo -p 27017:27017 --cpus=2.0 --memory=3g mongo:4.4 --wiredTigerCacheSizeGB 2.0
+docker pull mongo:8.2
+docker run --name local-mongo -p 27017:27017 --cpus=2.0 --memory=4g mongo:8.2 --wiredTigerCacheSizeGB 3.0
 #docker run --name local-mongo -p 27017:27017 --cpus=1.0 --memory=2g -v <mongo-dir>:/data/db mongo:6.0 --wiredTigerCacheSizeGB 1.0
 #docker start local-mongo
 #docker stop local-mongo
