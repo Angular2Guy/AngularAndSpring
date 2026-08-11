322bddff08122fa1eb42c928ca1d0a181bc89b82
Angular2Guy <angular2guy@gmail.com>
2026-08-11 20:56:20 +0200

    fix: config

--- runDocker.sh
+++ runDocker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker pull mongo:4.4
 docker run --name local-mongo -p 27017:27017 --cpus=2.0 --volume tmp/mongodb --memory=3g mongo:4.4 --wiredTigerCacheSizeGB 2.0
-docker run -p 8080:8080 --memory="1g" --network="host" angular2guy/angularandspring:latest
+docker run -p 8080:8080 --memory="1536m" --network="host" angular2guy/angularandspring:latest
